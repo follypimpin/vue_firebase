ad875c2c495f697bb7bc984b6d4d20ac6fe8a15b
Raphael <raphael@develodesign.co.uk>
2019-10-20 23:03:42 +0100

    Firebase Authentication

--- fireBaseConnector.js
+++ fireBaseConnector.js
@@ -1,1 +1,15 @@
-const config = require('./config');
+const firebaseConfig = {
+    apiKey: "AIzaSyAGcuA_mK7t5VwqFeFhM4Np4cQyvlvT7RM",
+    authDomain: "develo-vue.firebaseapp.com",
+    databaseURL: "https://develo-vue.firebaseio.com",
+    projectId: "develo-vue",
+    storageBucket: "develo-vue.appspot.com",
+    messagingSenderId: "1013582777342",
+    appId: "1:1013582777342:web:8a647069610f677a828eaf",
+    measurementId: "G-G2CTM3YNTL"
+};
+// Initialize Firebase
+const fireBase = firebase.initializeApp(firebaseConfig);
+
+//fireBase.analytics();
+export {fireBase}
